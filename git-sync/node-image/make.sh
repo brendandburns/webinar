9eb6b7f6c9bf48d2795633c2a6d42b6ca9140293
Brendan Burns <bburns@microsoft.com>
2018-04-23 21:59:46 -0700

    Updates.

--- git-sync/node-image/make.sh
+++ git-sync/node-image/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker build -t brendanburns/node-sync .
-sudo docker push brendanburns/node-sync
+sudo docker build -t brendanburns/node-server:v1 .
+sudo docker push brendanburns/node-server:v1
